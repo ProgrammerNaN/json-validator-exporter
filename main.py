4d0a39efb2a9d4f06cc848efde9a75c1776558db
Kristina Ilina <Christinamidala@gmail.com>
2022-10-19 12:43:55 +0300

    add get request

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import logging
 import settings
 import sys
+import requests
 
 def configure():
-    if (len(sys.argv) == 2):
+    if (len(sys.argv) == 3):
         return settings.load_settings(sys.argv[1])
     else:
         return settings.load_settings()
 
 def main():
+    link()
     logging.basicConfig(level=logging.INFO)
     config = configure() 
     print(config.get_metrics_for_module('default'))
+
+def link():
+    logging.basicConfig(level=logging.INFO)
+    req = requests.get(sys.argv[2])
+    if req.status_code == 200:
+        print(req.json())
+    else:
+        print(req.status_code)
 
 if __name__ == "__main__":
     main()
